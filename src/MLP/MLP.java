a3c0d6f7389980c8e1685a9c7418cb5c31fa2636
ZygmuntJakub <jakub.zygmunt.afm@gmail.com>
2019-04-07 19:56:04 +0200

    Naprawa nauki sieci, zmiana z Vector na array w odczytywaniu pliku

--- src/MLP/MLP.java
+++ src/MLP/MLP.java
@@ -1,15 +1,9 @@
 package MLP;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Vector;
 
 public class MLP {
 	public Layer[] layers;
-	public Vector<Vector<Integer>> data;
 	public boolean orBias;
 	public double newError = 1.0;
 
@@ -18,39 +12,20 @@ public class MLP {
 		layers = new Layer[3];
 		Parameters.learningRate = 0.1;
 		Parameters.momentum = 0.6;
-		getData();
 		layers[0] = new Layer(4, 0);
 		layers[1] = new Layer(3, 4);
 		layers[2] = new Layer(4, 3);
 	}
 
-	public void getData() throws IOException {
-		data = new Vector<>();
-		InputStream is = new FileInputStream("transformation.txt");
-		BufferedReader buf = new BufferedReader(new InputStreamReader(is));
-		String line = buf.readLine();
-		while (line != null) {
-			Vector<Integer> values = new Vector<>();
-			values.add(Integer.parseInt(line.split(" ")[0]));
-			values.add(Integer.parseInt(line.split(" ")[1]));
-			values.add(Integer.parseInt(line.split(" ")[2]));
-			values.add(Integer.parseInt(line.split(" ")[3]));
-			data.add(values);
-			line = buf.readLine();
-		}
-		buf.close();
-	}
 
-	public Vector<Vector<Double>> execute() {
-		Vector<Vector<Double>> result = new Vector<>();
+
+	public double [] execute(double [] inputs) {
+		double [] result = new double[inputs.length];
 		double newOutput = 0.0;
-		for (int d = 0; d < data.size(); d++) {
-
-			Vector<Double> res = new Vector<>();
 
 			// Wprowadzamy wartości wejściowe
 			for (int i = 0; i < layers[0].size; i++) {
-				layers[0].neurons[i].output = data.get(d).get(i);
+				layers[0].neurons[i].output = inputs[i];
 			}
 			// obliczamy wartości wyjściowe dla każdego neuronu z warstwy ukrytej
 			for (int k = 1; k < layers.length; k++) {
@@ -66,23 +41,20 @@ public class MLP {
 			}
 			// wpisujemy do wektora wyliczone wartości wyjściowe
 			for (int i = 0; i < layers[2].size; i++) {
-				res.add(layers[2].neurons[i].output);
+				result[i] =  layers[2].neurons[i].output;
 			}
-			result.add(res);
-		}
 		return result;
 	}
 
-	public void backPropagate() {
-		Vector<Vector<Double>> outputs = execute();
+	public void backPropagate(double [] outputs, double []inputs) {
+		double[] newOutputs = execute(inputs);
 		double error = 0.0;
-
-		for (int x = 0; x < outputs.size(); x++) {
+		newError = 0.0;
 
 			// wyznaczamy błąd na wyjściu
 			for (int i = 0; i < layers[2].size; i++) {
-				error = outputs.get(x).get(i) - data.get(x).get(i);
-				layers[2].neurons[i].delta = error * SigmoidalFunction.sigmoidDerivative(outputs.get(x).get(i));
+				error = newOutputs[i] - outputs[i];
+				layers[2].neurons[i].delta = error * SigmoidalFunction.sigmoidDerivative(outputs[i]);
 			}
 
 			for (int k = layers.length - 2; k >= 0; k--) {
@@ -113,14 +85,12 @@ public class MLP {
 			}
 
 			newError = 0.0;
-			for (int i = 0; i < outputs.get(0).size(); i++) {
-				newError += Math.pow(outputs.get(x).get(i) - data.get(x).get(i), 2);
+			for (int i = 0; i < outputs.length; i++) {
+				newError += Math.pow(newOutputs[i] - outputs[i], 2);
 			}
-			newError = newError / (2.0 * outputs.get(0).size());
 
 			System.out.println(newError);
 
-		}
 
 	}
 }
--- src/MLP/Test.java
+++ src/MLP/Test.java
@@ -1,20 +1,62 @@
 package MLP;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Vector;
 
 public class Test {
+	public static Vector<Vector<Integer>> data;
+	
+	public static void getData() throws IOException {
+		data = new Vector<>();
+		InputStream is = new FileInputStream("transformation.txt");
+		BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+		String line = buf.readLine();
+		while (line != null) {
+			Vector<Integer> values = new Vector<>();
+			values.add(Integer.parseInt(line.split(" ")[0]));
+			values.add(Integer.parseInt(line.split(" ")[1]));
+			values.add(Integer.parseInt(line.split(" ")[2]));
+			values.add(Integer.parseInt(line.split(" ")[3]));
+			data.add(values);
+			line = buf.readLine();
+		}
+		buf.close();
+	}
 
 	public static void main(String[] args) throws IOException {
 		MLP test  = new MLP(3);
+		double [] getData1 = {1, 0, 0, 0 };
+		double [] getData2 = {0, 1, 0, 0 };
+		double [] getData3 = {0, 0, 1, 0 };
+		double [] getData4 = {0, 0, 0, 1 };
 		
-//		while(test.newError > 0.05) {
-//			test.backPropagate();
-//		}
-		for (int i = 0; i < 100000; i++) {
-			test.backPropagate();
-			
+		double backPropagateError = 10.0;
+		while(backPropagateError > 0.000001) {
+			backPropagateError = 0;
+			test.backPropagate(getData1, getData1);
+			backPropagateError += test.newError;
+			test.backPropagate(getData2, getData2);
+			backPropagateError += test.newError;
+			test.backPropagate(getData3, getData3);
+			backPropagateError += test.newError;
+			test.backPropagate(getData4, getData4);
+			backPropagateError += test.newError;
+			backPropagateError = backPropagateError / (2 * getData1.length);
 		}
-		System.out.println(test.execute());
+		System.out.println(Arrays.toString(test.execute(getData1)));
+		System.out.println(Arrays.toString(test.execute(getData2)));
+		System.out.println(Arrays.toString(test.execute(getData3)));
+		System.out.println(Arrays.toString(test.execute(getData4)));
+//		for (int i = 0; i < 250000; i++) {
+//			test.backPropagate();
+//			
+//		}
+//		System.out.println(test.execute());
 
 		
 	}
